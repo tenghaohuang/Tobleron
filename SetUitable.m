5b911125fdb272d26f96f2470b12e3cbd0a8dc54
Miller Lab Member <millerlab@big-mac.wireless-1x.unc.edu>
2019-11-18 12:37:33 -0500

    11.18
    lol

--- SetUitable.m
+++ SetUitable.m
@@ -1,5 +1,5 @@
-function [bool,handle] = SetUitable(data)
-figure('Name','DataTable');
+function [bool,handle,figure_handle] = SetUitable(data)
+figure_handle=figure('Name','DataTable');
 uitable_handle=uitable();
 set(uitable_handle,'data',{});
 set(uitable_handle,'data',data);
