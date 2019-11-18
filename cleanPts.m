bf8048239bfe496de261da5a65591f557c7919de
Miller Lab Member <millerlab@big-mac.wireless-1x.unc.edu>
2019-11-18 11:50:18 -0500

    11.18
    fix most thins

--- cleanPts.m
+++ cleanPts.m
@@ -1,9 +1,21 @@
 function [points]=cleanPts(pts)
     len = size(pts,2);
+    counter =1;
+    global index;
+    index = [];
 for i =1:len
-    if(i<=size(pts,2))
-        if(pts(1,i)==pts(2,i))
-            pts(:,i)=[];
+    
+        if(pts(1,i)==pts(2,i)&&pts(1,i)==0)
+           index(counter) = i;
+           counter = counter+1;
+        end
+        
+end
+for i = 1:size(index,2)
+    if(index(i)~=0)
+    pts(:,index(i))=[];
+        for j=i+1:size(index,2)
+            index(j)=index(j)-1;
         end
     end
 end
